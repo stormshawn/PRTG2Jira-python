b9878c563174bef27164ecc7e1de4b98ecd969f5
Shawn Storm <Shawn.Storm@medialine.ag>
2026-03-08 20:41:59 +0100

    JiraService needs a bit of review which I will do in the next week. PRTGservice is finished. Beginning with now the config file

--- app/services/JiraService.py
+++ app/services/JiraService.py
@@ -2,7 +2,7 @@ from typing import Any, Dict, List, Optional
 import httpx
 import logging
 
-from app.models import InsightDto, IssueDto, PropertyDto
+from app.models import InsightDto, IssueDto, JiraProjectSettingsDto, PropertyDto
 from app.models.CommentParametersDto import CommentParametersDto
 from app.models.ValueDto import ValueDto
 logger: logging.Logger = logging.getLogger(__name__)
@@ -245,15 +245,107 @@ class Jiraservice():
                     logger.debug(f"{ticket_parameters}")
                     return "-1"
             else:
-                logger.error(f"Error on invoking of NewJiraTicket to create the Ticket. {response}")
+                logger.error(f"Error on invoking of new_jira_ticket to create the Ticket. {response}")
                 logger.debug(f"{ticket_parameters}")
                 return "-1"
         except Exception as e:
-            logger.error(f"Error on invoking of NewJiraTicket to create the Ticket. {response}")
+            logger.error(f"Error on invoking of new_jira_ticket to create the Ticket. {response}")
             logger.debug(f"{ticket_parameters}")
             return "-1"
     
+    async def update_jira_ticket(
+            self, 
+            jira_instance: str,
+            ticket_id: str,
+            crm_key: str,
+            reporter: str
+            )-> str:
+        
 
 
+        base_url: str = "Get it from config later"
+        token: str = "Test token"
+        headers: Dict[str, Any] = {
+        "Authorization": f"Bearer {token}", 
+        "Content-Type": "application/json"
+        }
 
 
+        ticket_parameters: Dict[str, Any] = {
+
+            "fields": {
+                "customfield_10504": [{"key": crm_key}],
+                "labels": ["PRTG", "Monitoring"],
+                "reporter": {"name": reporter}
+            }
+        }
+
+        try: 
+            response: httpx.Response = await self.http_client.put(
+                f"https://{base_url}/rest/api/latest/issue/{ticket_id}",
+                json=ticket_parameters,
+                headers=headers)
+            if response.status_code in [200, 204]:
+                return 0
+            else: 
+                logger.error(f"Error on invoking of update_jira_ticket to update the Ticket. {response}")
+                logger.debug(f"{ticket_parameters}")
+            return -1
+        except Exception as e:
+            logger.error(f"Error on invoking of UpdateJiraTicket to update the Ticket: {e}")
+            logger.debug(f"{ticket_parameters}")
+            return -2
+        
+    def get_from_tags(self, tags: str, instance: str, setting: str)-> str:
+        project_keys: Dict[str, Any]  #get it from config later
+        tag_list: List[str] = tags.split()
+
+        try:
+            matched_value: Optional[str]  = None
+            for key, value in project_keys.items():
+                if key.lower() == "default":
+                    continue
+                if any (tag.lower() in key.lower for tag in tag_list):
+                    matched_value = str(value) if value else None
+                    break
+            if matched_value:
+                return matched_value
+            default_value: Optional[str] = None
+            if isinstance(project_keys, dict) and "default" in project_keys:
+                default_value = str(project_keys["default"]) if project_keys["default"] else None
+            return default_value if default_value else ""
+        except Exception as e:
+            default_value: Optional[str] = None
+            if isinstance(project_keys, dict) and "default" in project_keys:
+                default_value = str(project_keys["default"]) if project_keys["default"] else None
+                return default_value if default_value else ""
+            return ""
+
+    def get_project_from_tags(self, tags: str, instance: str, setting: str)-> JiraProjectSettingsDto:
+        section: Dict[str, Any] #define after having config file
+        tag_list: List[str] = tags.split()
+        try:
+            matched_section: Dict[str, Any] = None
+            for key, value in section.items():
+                if key.lower() == "default":
+                    continue
+                if any (tag.lower() in key.lower for tag in tag_list):
+                    matched_section = value if isinstance(value, dict) else {}
+                    break
+            if matched_section is None:
+                default_section: Optional[Dict[str, Any]] = section.items("default")
+            if default_section and isinstance(default_section, dict):
+                matched_section = default_section
+            if matched_section is not None:
+                settings: JiraProjectSettingsDto = JiraProjectSettingsDto()
+                settings.
+                for key, value in matched_section.items():
+                    key_lower: str = key.lower
+                    if key_lower == "projectkey":
+                        settings.Pro
+                    elif key_lower == "servicedesk":
+                    elif key_lower == "servicedeskid":
+                    elif key_lower == "requesttypeid":
+                    
+
+        except Exception as e:
